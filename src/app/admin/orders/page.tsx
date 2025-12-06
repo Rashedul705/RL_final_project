98a05f20d54eeabf3098a52545f2b53c22d430bb
Rashedul Islam <rashedul.afl@gmail.com>
2025-12-06 22:01:36 +0000

    Try fixing this error: `Runtime ReferenceError: DropdownMenu is not defi

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -30,6 +30,12 @@ import {
     DialogFooter,
 } from "@/components/ui/dialog";
 import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
     Select,
     SelectContent,
     SelectItem,
