e8ad5a1a842b4e89027060c71dcbe8c211692039
Rashedul Islam <rashedul.afl@gmail.com>
2025-12-07 19:56:36 +0000

    total sales (_for element <CardTitle>_)

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -83,7 +83,7 @@ export default function AdminDashboardPage() {
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mt-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+            <CardTitle className="text-sm font-medium">Total Sales</CardTitle>
             <CircleDollarSign className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
