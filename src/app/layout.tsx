d8cb0df42b53aa055aa05d31a1b12a52b1e098dc
Rashedul <rashedul.afl@gmail.com>
2026-02-01 02:19:08 +0600

    gtm installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
